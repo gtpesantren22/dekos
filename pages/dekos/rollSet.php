90b83aaf18798747d29777091cc95f8319754a17
gtpesantren22 <cakromat26@gmail.com>
2023-01-02 22:08:24 +0700

    tambah sinkron ke dpontren

--- pages/dekos/rollSet.php
+++ pages/dekos/rollSet.php
@@ -15,7 +15,6 @@ if ($kd === 'dekos') {
 }
 
 if ($kd === 'ulang') {
-
     mysqli_query($conn, "TRUNCATE tb_santri");
     // $sql = mysqli_query($conn2, "INSERT INTO db_dekos.tb_santri SELECT * FROM db_santri.tb_santri ");
     $sql = mysqli_query($conn2, "INSERT INTO u9048253__dekos.tb_santri SELECT * FROM u9048253__santri.tb_santri ");
@@ -23,6 +22,23 @@ if ($kd === 'ulang') {
         echo "
         <script>
             alert('Data sudah dimuat ulang');
+            window.location = 'index.php?link=pages/dekos/roll';
+        </script>
+        ";
+    }
+}
+
+if ($kd === 'dpontren') {
+    $dt = mysqli_query($conn, "SELECT * FROM kosmen");
+    while ($kd = mysqli_fetch_assoc($dt)) {
+        $tks = $kd['t_kos'];
+        $nis = $kd['nis'];
+        $sql = mysqli_query($conn2, "UPDATE tb_santri SET t_kos = $tks WHERE nis = '$nis' ");
+    }
+    if ($sql) {
+        echo "
+        <script>
+            alert('Data sudah disinkron ke dpontren');
             window.location = 'index.php?link=pages/dekos/roll';
         </script>
         ";
