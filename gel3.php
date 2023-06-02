e7a066ec7ceb4684f4e9c5cf7d64f656692b0214
gtpesantren22 <cakromat26@gail.com>
2023-06-02 20:55:22 +0700

    tambah data gel3

---
+++ gel3.php
@@ -0,0 +1,46 @@
+<?php
+
+$conn = mysqli_connect("localhost", "u9048253_dwk", "PesantrenDWKIT2021", "u9048253_psb23");
+$data = mysqli_query($conn, "SELECT * FROM tb_santri WHERE ket = 'baru' AND gel = 3 ORDER BY nama ASC ");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Gelombang 3</title>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>NO</th>
+                <th>NIS</th>
+                <th>NAMA</th>
+                <th>ALAMAT</th>
+                <th>JKL</th>
+                <th>LEMBAGA</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $no = 1;
+            while ($d = mysqli_fetch_array($data)) : ?>
+                <tr>
+                    <td><?= $no++ ?></td>
+                    <td><?= $d['nis'] ?></td>
+                    <td><?= $d['nama'] ?></td>
+                    <td><?= $d['desa'] . '-' . $d['kec'] . '-' . $d['kab'] ?></td>
+                    <td><?= $d['jkl'] ?></td>
+                    <td><?= $d['lembaga'] ?></td>
+                </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
