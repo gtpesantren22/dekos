0b764ef84b071dbf605a06646f90bfe18b01a219
cakromat26 <cakromat26@gmail.com>
2025-10-04 14:57:13 +0700

    update: add load file ajax

---
+++ ajax/cek_kos.php
@@ -0,0 +1,313 @@
+<?php
+include '../function.php';
+
+$kelas = $_POST['k_formal'];
+$klsK = explode('-', $kelas);
+
+$kls = $klsK[0];
+$jur = $klsK[1];
+$rom = $klsK[2];
+$tingkat = $_POST['t_formal'];
+
+$bulan = $_POST['bulan'];
+$tahun = $_POST['tahun'];
+
+if ($bulan == 1) {
+    $bl = "Januari";
+} elseif ($bulan == 2) {
+    $bl = "Feburari";
+} elseif ($bulan == 3) {
+    $bl = "Maret";
+} elseif ($bulan == 4) {
+    $bl = "April";
+} elseif ($bulan == 5) {
+    $bl = "Mei";
+} elseif ($bulan == 6) {
+    $bl = "juni";
+} elseif ($bulan == 7) {
+    $bl = "Juli";
+} elseif ($bulan == 8) {
+    $bl = "Agustus";
+} elseif ($bulan == 9) {
+    $bl = "September";
+} elseif ($bulan == 10) {
+    $bl = "Oktober";
+} elseif ($bulan == 11) {
+    $bl = "November";
+} elseif ($bulan == 12) {
+    $bl = "Desember";
+}
+?>
+
+<div class="col-md-12">
+    <div class="box box-primary">
+        <div class="box-header">
+            <h3 class="box-title">
+                Data dari :
+                <br />
+                <br />
+                <p class="label label-primary"> KELAS : <?= $kelas; ?> <?= $tingkat; ?></p>
+                <p class="label label-warning"> BULAN : <?= $bl; ?></p>
+                <p class="label label-danger"> TAHUN : <?= $tahun; ?></p>
+            </h3>
+            <a href="<?= 'pages/rekap/excel_kos.php?dari=' . $dari . '&sampai=' . $sampai ?>" target="_blank" type="button" class="btn btn-success pull-right"><span class="fa fa-download">
+                </span>
+                Download excel</a>
+        </div>
+        <hr>
+        <div class="box-body">
+            <?php
+            mysqli_query($conn, "DROP VIEW IF EXISTS rekap");
+            mysqli_query($conn, "DROP VIEW IF EXISTS rekap2");
+
+            mysqli_query($conn, "CREATE VIEW rekap AS SELECT SUM(a.nominal) AS jml, b.nama, b.k_formal, b.nis, b.t_formal, b.r_formal, b.jurusan FROM kos a 
+                            JOIN kunci b ON a.nis = b.nis WHERE b.k_formal = '$kls' AND b.t_formal = '$tingkat' AND b.r_formal = '$rom' AND b.jurusan = '$jur' 
+                            AND a.bulan = $bulan AND a.tahun = '$tahun' AND b.bulan = $bulan AND b.tahun = '$tahun' AND ket = 0 GROUP BY nis ORDER BY nominal ASC");
+
+            mysqli_query($conn, "CREATE VIEW rekap2 AS SELECT nama, nis FROM kunci WHERE k_formal = '$kls' AND t_formal = '$tingkat' AND r_formal = '$rom' AND jurusan = '$jur' AND ket = 0  AND bulan = $bulan AND tahun = '$tahun' ");
+
+            $lunas = mysqli_query($conn, "SELECT * FROM rekap WHERE jml >= 300000 ");
+            $belum = mysqli_query($conn, "SELECT * FROM rekap WHERE jml < 300000 ");
+            $tidak = mysqli_query($conn, "SELECT * FROM rekap2 WHERE NOT EXISTS (SELECT * FROM rekap WHERE rekap2.nis = rekap.nis) ");
+            ?>
+            <center>
+                <h3><span class="label label-success">Data sudah lunas</span></h3>
+            </center>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped" id="example1_bst">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Kelas</th>
+                            <th>Bulan</th>
+                            <th>Jumlah Bayar</th>
+                            <th>Kurang</th>
+                            <th>Ket</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $i = 1;
+                        ?>
+                        <?php foreach ($lunas as $r) : ?>
+                            <?php
+                            if ($bulan == 1) {
+                                $bl = "Januari";
+                            } elseif ($bulan == 2) {
+                                $bl = "Feburari";
+                            } elseif ($bulan == 3) {
+                                $bl = "Maret";
+                            } elseif ($bulan == 4) {
+                                $bl = "April";
+                            } elseif ($bulan == 5) {
+                                $bl = "Mei";
+                            } elseif ($bulan == 6) {
+                                $bl = "juni";
+                            } elseif ($bulan == 7) {
+                                $bl = "Juli";
+                            } elseif ($bulan == 8) {
+                                $bl = "Agustus";
+                            } elseif ($bulan == 9) {
+                                $bl = "September";
+                            } elseif ($bulan == 10) {
+                                $bl = "Oktober";
+                            } elseif ($bulan == 11) {
+                                $bl = "November";
+                            } elseif ($bulan == 12) {
+                                $bl = "Desember";
+                            }
+                            ?>
+                            <tr>
+                                <td><?= $i; ?></td>
+                                <td><?= $r['nama']; ?></td>
+                                <td><?= $r['k_formal']; ?> <?= $r['t_formal']; ?></td>
+                                <td><?= $bl; ?> <?= $tahun; ?></td>
+                                <td><?= rupiah($r['jml']); ?></td>
+                                <?php $j = 300000 - $r['jml']; ?>
+                                <td><?= rupiah($j); ?></td>
+                                <td style="font-weight: bold; color: green;"><span class="fa fa-check"></span> Lunas
+                                </td>
+                            </tr>
+                            <?php $i++; ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+            <hr>
+            <center>
+                <h3><span class="label label-warning">Data Belum Lunas</span></h3>
+            </center>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped" id="example2_bst">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Kelas</th>
+                            <th>Bulan</th>
+                            <th>Jumlah Bayar</th>
+                            <th>Kurang</th>
+                            <th>Ket</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $i = 1;
+                        ?>
+                        <?php foreach ($belum as $r) : ?>
+                            <?php
+                            if ($bulan == 1) {
+                                $bl = "Januari";
+                            } elseif ($bulan == 2) {
+                                $bl = "Feburari";
+                            } elseif ($bulan == 3) {
+                                $bl = "Maret";
+                            } elseif ($bulan == 4) {
+                                $bl = "April";
+                            } elseif ($bulan == 5) {
+                                $bl = "Mei";
+                            } elseif ($bulan == 6) {
+                                $bl = "juni";
+                            } elseif ($bulan == 7) {
+                                $bl = "Juli";
+                            } elseif ($bulan == 8) {
+                                $bl = "Agustus";
+                            } elseif ($bulan == 9) {
+                                $bl = "September";
+                            } elseif ($bulan == 10) {
+                                $bl = "Oktober";
+                            } elseif ($bulan == 11) {
+                                $bl = "November";
+                            } elseif ($bulan == 12) {
+                                $bl = "Desember";
+                            }
+                            ?>
+                            <tr>
+                                <td><?= $i; ?></td>
+                                <td><?= $r['nama']; ?></td>
+                                <td><?= $r['k_formal']; ?> <?= $r['t_formal']; ?></td>
+                                <td><?= $bl; ?> <?= $tahun; ?></td>
+                                <td style="font-weight: bold; color: orange;"><?= rupiah($r['jml']); ?></td>
+                                <?php $j = 300000 - $r['jml']; ?>
+                                <td style="font-weight: bold; color: orange;"><?= rupiah($j); ?></td>
+                                <td style="font-weight: bold; color: orange;"><span class="fa fa-refresh"></span>
+                                    Blm
+                                    Lunas
+                                </td>
+                            </tr>
+                            <?php $i++; ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+            <hr>
+            <center>
+                <h3><span class="label label-danger">Data Belum Bayar</span></h3>
+            </center>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped" id="example3_bst">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Kelas</th>
+                            <th>Bulan</th>
+                            <th>Jumlah Bayar</th>
+                            <th>Kurang</th>
+                            <th>Ket</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $i = 1;
+                        ?>
+                        <?php foreach ($tidak as $r) : ?>
+                            <?php
+                            if ($bulan == 1) {
+                                $bl = "Januari";
+                            } elseif ($bulan == 2) {
+                                $bl = "Feburari";
+                            } elseif ($bulan == 3) {
+                                $bl = "Maret";
+                            } elseif ($bulan == 4) {
+                                $bl = "April";
+                            } elseif ($bulan == 5) {
+                                $bl = "Mei";
+                            } elseif ($bulan == 6) {
+                                $bl = "juni";
+                            } elseif ($bulan == 7) {
+                                $bl = "Juli";
+                            } elseif ($bulan == 8) {
+                                $bl = "Agustus";
+                            } elseif ($bulan == 9) {
+                                $bl = "September";
+                            } elseif ($bulan == 10) {
+                                $bl = "Oktober";
+                            } elseif ($bulan == 11) {
+                                $bl = "November";
+                            } elseif ($bulan == 12) {
+                                $bl = "Desember";
+                            }
+                            ?>
+                            <tr>
+                                <td><?= $i; ?></td>
+                                <td><?= $r['nama']; ?></td>
+                                <td><?= $kelas; ?> <?= $tingkat; ?></td>
+                                <td><?= $bl; ?> <?= $tahun; ?></td>
+                                <td style="font-weight: bold; color: red;"> -</td>
+                                <td style="font-weight: bold; color: red;"> -</td>
+                                <td style="font-weight: bold; color: red;"><span class="fa fa-close"></span> Tak
+                                    Bayar
+                                </td>
+                            </tr>
+                            <?php $i++; ?>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div><!-- /.box-body -->
+        <div class="row">
+            <?php $total = mysqli_num_rows($lunas) + mysqli_num_rows($belum) + mysqli_num_rows($tidak); ?>
+            <div class="col-xs-6 pull-right">
+                <div class="table-responsive">
+                    <table class="table">
+                        <tr>
+                            <td><?= rupiah(mysqli_num_rows($lunas) * 300000); ?></td>
+                        </tr>
+                        <tr>
+                            <td><?= rupiah(mysqli_num_rows($belum) * 300000); ?></td>
+                        </tr>
+                        <tr>
+                            <td><?= rupiah(mysqli_num_rows($tidak) * 300000); ?></td>
+                        </tr>
+                        <tr>
+                            <th><?= rupiah($total * 300000); ?>
+                            </th>
+                        </tr>
+                    </table>
+                </div>
+            </div><!-- /.col -->
+            <div class="col-xs-6 pull-right">
+                <div class="table-responsive">
+                    <table class="table">
+                        <tr>
+                            <th>Lunas</th>
+                            <td><?= mysqli_num_rows($lunas); ?></td>
+                        </tr>
+                        <tr>
+                            <th>Belum Lunas</th>
+                            <td><?= mysqli_num_rows($belum); ?></td>
+                        </tr>
+                        <tr>
+                            <th>Tidak Bayar</th>
+                            <td><?= mysqli_num_rows($tidak); ?></td>
+                        </tr>
+                        <tr>
+                            <th style="width:50%">TOTAL</th>
+                            <th><?= $total; ?>
+                            </th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div><!-- /.row -->
+    </div>
+</div>
--- pages/dekos/cek_kos2.php
+++ pages/dekos/cek_kos2.php
@@ -1,4 +1,3 @@
-
 <section class="content-header">
     <h1><span class="fa fa-cutlery"> </span>
         Cek Data Dekosan (Sluruh Santri)
@@ -17,7 +16,7 @@
                 <div class="box-header">
                 </div>
                 <div class="box-body">
-                    <form action="" method="post">
+                    <form id="form-cari" action="" method="post">
                         <!-- Date range -->
                         <div class="col-md-2">
                             <div class="form-group">
@@ -71,8 +70,8 @@
                                     while ($thn = mysqli_fetch_array($th)) {
                                         $no++;
                                     ?>
-                                    <option value="<?= $thn['nama'] ?>"><?= $thn['nama'] ?>
-                                    </option>
+                                        <option value="<?= $thn['nama'] ?>"><?= $thn['nama'] ?>
+                                        </option>
                                     <?php
                                     }
                                     ?>
@@ -90,200 +89,30 @@
                 </div>
             </div>
         </div>
-        <?php
-        if (isset($_POST['cari'])) {
-            $jkl = $_POST['jkl'];
-            $stts = $_POST['stts'];
-            $bulan = $_POST['bulan'];
-            $tahun = $_POST['tahun'];
-
-            if ($bulan == 1) {
-                $bl = "Januari";
-            } elseif ($bulan == 2) {
-                $bl = "Feburari";
-            } elseif ($bulan == 3) {
-                $bl = "Maret";
-            } elseif ($bulan == 4) {
-                $bl = "April";
-            } elseif ($bulan == 5) {
-                $bl = "Mei";
-            } elseif ($bulan == 6) {
-                $bl = "juni";
-            } elseif ($bulan == 7) {
-                $bl = "Juli";
-            } elseif ($bulan == 8) {
-                $bl = "Agustus";
-            } elseif ($bulan == 9) {
-                $bl = "September";
-            } elseif ($bulan == 10) {
-                $bl = "Oktober";
-            } elseif ($bulan == 11) {
-                $bl = "November";
-            } elseif ($bulan == 12) {
-                $bl = "Desember";
-            }
-
-            mysqli_query($conn, "DROP VIEW IF EXISTS rekap");
-            mysqli_query($conn, "DROP VIEW IF EXISTS rekap2");
-
-            mysqli_query($conn, "CREATE VIEW rekap AS SELECT SUM(a.nominal) AS jml, b.nama, b.nis, b.k_formal, b.t_formal, b.kamar FROM kos AS a 
-                            INNER JOIN kunci AS b ON a.nis = b.nis WHERE a.bulan = $bulan AND a.tahun = '$tahun' AND b.bulan = $bulan AND b.tahun = '$tahun' AND ket = 0 AND jkl = '$jkl' GROUP BY nis ORDER BY nominal ASC");
-
-            mysqli_query($conn, "CREATE VIEW rekap2 AS SELECT nama, nis, k_formal, t_formal, kamar FROM kunci WHERE ket = 0 AND jkl = '$jkl' AND bulan = $bulan AND tahun = '$tahun' ");
-            $san = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM kunci"));
-
-            if ($stts == 1) {
-                $data = mysqli_query($conn, "SELECT * FROM rekap WHERE jml >= 300000 ");
-            } elseif ($stts == 2) {
-                $data = mysqli_query($conn, "SELECT * FROM rekap WHERE jml < 300000 ");
-            } elseif ($stts == 3) {
-                $data = mysqli_query($conn, "SELECT * FROM rekap2 WHERE NOT EXISTS (SELECT * FROM rekap WHERE rekap2.nis = rekap.nis) ");
-            }
-        ?>
-        <form action="" method="post">
-            <div class="col-md-12">
-                <div class="box box-primary">
-                    <div class="box-header">
-                        <h3 class="box-title">
-                            Data dari :
-                            <br />
-                            <br />
-                            <?php if ($stts == 1) {
-                                    $st = "LUNAS";
-                                } elseif ($stts == 2) {
-                                    $st = "BELUM LUNAS";
-                                } elseif ($stts == 3) {
-                                    $st = "TAK BAYAR";
-                                } ?>
-                            <p class="label label-primary"> STATUS : <?= $st; ?> </p>
-                            <p class="label label-warning"> BULAN : <?= $bl; ?></p>
-                            <p class="label label-danger"> TAHUN : <?= $tahun; ?></p>
-                        </h3>
-                        <a href="<?= 'pages/rekap/excel_kos.php?dari=' . $dari . '&sampai=' . $sampai ?>"
-                            target="_blank" type="button" class="btn btn-success pull-right"><span
-                                class="fa fa-download">
-                            </span>
-                            Download excel</a>
-                    </div>
-                    <div class="box-header">
-                        <div class="col-xs-5">
-                            <table class="table">
-                                <tr>
-                                    <th>Jumlah</th>
-                                    <th><?= mysqli_num_rows($data); ?> santri</th>
-                                </tr>
-                                <tr>
-                                    <th>Persentase</th>
-                                    <?php
-                                        $persen = (mysqli_num_rows($data) / $san) * 100;
-                                        ?>
-                                    <th><?= round($persen, 2); ?> %</th>
-                                </tr>
-                                <tr>
-                                    <th style="width:50%">TOTAL</th>
-                                    <th><?= rupiah(mysqli_num_rows($data) * 300000); ?></th>
-                                </tr>
-                            </table>
-                        </div>
-                    </div>
-                    <div class="box-body">
-                        <div class="table-responsive">
-                            <table class="table table-bordered table-striped" id="example1_bst">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Nama</th>
-                                        <th>Kelas</th>
-                                        <th>Kamar</th>
-                                        <th>Bulan</th>
-                                        <th>Jumlah Bayar</th>
-                                        <th>Kurang</th>
-                                        <th>Ket</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                        $i = 1;
-                                        ?>
-                                    <?php foreach ($data as $r) : ?>
-                                    <?php
-                                            if ($bulan == 1) {
-                                                $bl = "Januari";
-                                            } elseif ($bulan == 2) {
-                                                $bl = "Feburari";
-                                            } elseif ($bulan == 3) {
-                                                $bl = "Maret";
-                                            } elseif ($bulan == 4) {
-                                                $bl = "April";
-                                            } elseif ($bulan == 5) {
-                                                $bl = "Mei";
-                                            } elseif ($bulan == 6) {
-                                                $bl = "juni";
-                                            } elseif ($bulan == 7) {
-                                                $bl = "Juli";
-                                            } elseif ($bulan == 8) {
-                                                $bl = "Agustus";
-                                            } elseif ($bulan == 9) {
-                                                $bl = "September";
-                                            } elseif ($bulan == 10) {
-                                                $bl = "Oktober";
-                                            } elseif ($bulan == 11) {
-                                                $bl = "November";
-                                            } elseif ($bulan == 12) {
-                                                $bl = "Desember";
-                                            }
-                                            ?>
-                                    <tr>
-                                        <td><?= $i; ?></td>
-                                        <td><?= $r['nama']; ?></td>
-                                        <td><?= $r['k_formal']; ?> <?= $r['t_formal']; ?></td>
-                                        <td><?= $r['kamar']; ?></td>
-                                        <td><?= $bl; ?> <?= $tahun; ?></td>
-
-                                        <?php if ($stts == 1) : ?>
-                                        <td><?= rupiah($r['jml']); ?></td>
-                                        <?php $j = 300000 - $r['jml']; ?>
-                                        <td><?= rupiah($j); ?></td>
-                                        <td style="font-weight: bold; color: green;"><span class="fa fa-check"></span>
-                                            Lunas
-                                        </td>
-                                        <?php endif ?>
-
-                                        <?php if ($stts == 2) : ?>
-                                        <td style="font-weight: bold; color: orange;"><?= rupiah($r['jml']); ?></td>
-                                        <?php $j = 300000 - $r['jml']; ?>
-                                        <td style="font-weight: bold; color: orange;"><?= rupiah($j); ?></td>
-                                        <td style="font-weight: bold; color: orange;"><span class="fa fa-refresh"> Blm
-                                                Lunas</span>
-                                        </td>
-                                        <?php endif ?>
-
-                                        <?php if ($stts == 3) : ?>
-                                        <td style="font-weight: bold; color: red;"> -</td>
-                                        <td style="font-weight: bold; color: red;"> -</td>
-                                        <td style="font-weight: bold; color: red;"><span class="fa fa-close"></span> Tak
-                                            Bayar
-                                        </td>
-                                        <?php endif ?>
-                                    </tr>
-                                    <?php $i++; ?>
-                                    <?php endforeach; ?>
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <th colspan="4">TOTAL</th>
-                                        <th>TOTAL</th>
-                                        <th>TOTAL</th>
-                                        <th></th>
-                                    </tr>
-                                </tfoot>
-                            </table>
-                        </div>
-                    </div><!-- /.box-body -->
-                </div>
-        </form>
-        <?php
-        }
-        ?>
+        <!-- Hasil -->
+        <div id="show-data"></div>
     </div>
 </section>
+<script>
+    $('#form-cari').on('submit', function(e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: 'ajax/cek_kos2.php',
+            type: 'POST',
+            data: $(this).serialize(),
+            dataType: 'html',
+            beforeSend: function() {
+                // (opsional) tampilkan loading
+                $('#show-data').html('<div class="text-center text-gray-500">Memuat data...</div>');
+            },
+            success: function(res) {
+                $('#show-data').html(res);
+            },
+            error: function(xhr, status, error) {
+                console.error('AJAX Error:', error);
+                $('#show-data').html('<div class="text-red-500 text-center">Terjadi kesalahan saat memuat data.</div>');
+            }
+        });
+    });
+</script>
